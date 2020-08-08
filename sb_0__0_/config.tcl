652f8ceceaa00e86b74c3293495ba15ea87ef9eb
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-08-08 20:57:56 +0000

    sb_0_0

---
+++ sb_0__0_/config.tcl
@@ -0,0 +1,18 @@
+# User config
+set ::env(DESIGN_NAME) sb_0__0_
+
+# Change if needed
+set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/sb_0__0_/src/*.v]
+
+
+# Fill this
+set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "clk"
+
+
+set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(PDK_VARIANT)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
+
+
