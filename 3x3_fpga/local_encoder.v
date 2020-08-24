29b300a638ada42fd91ba0181de8c91e5f0b0db2
ahmedafify <ahmed-afify@turkto.com>
2020-08-24 20:14:50 +0200

    3x3fpga

---
+++ 3x3_fpga/local_encoder.v
@@ -0,0 +1,10 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Local Decoders for Multiplexers
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Mon Aug 24 20:06:11 2020
+//-------------------------------------------
+//----- Time scale -----
+`timescale 1ns / 1ps
+
---
+++ 3x3_fpga/openlane_src_cells.v
@@ -0,0 +1,25 @@
+(* blackbox *)
+module sky130_fd_sc_hd__inv_1 (input A, output Y); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__inv_2 (input A, output Y); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__inv_4 (input A, output Y); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__buf_1(input A, output X); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__buf_2(input A, output X); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__buf_4(input A, output X); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__or2_1(input A, input B, output X); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__mux2_1(input A0, input A1, input S, output X); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__dfrtp_1(input CLK, input D, input RESETB, output Q); endmodule
+(* blackbox *)
+module sky130_fd_sc_hd__dfrbp_1(input CLK, input D, input RESETB, output Q, output QN); endmodule
+
+(* blackbox *)
+module iopad(input en, input inpad, output outpad, output pad); 
+sky130_fd_sc_hd__buf_2 sky130_fd_sc_hd__buf_2_io(.A(inpad), .X(outpad));
+endmodule
