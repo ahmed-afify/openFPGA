312aae38df9aa27c622653374ced2a6856e3cc3d
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-09-01 08:08:47 -0700

    sb_1__1_/

--- sb_1__1_/config.tcl
+++ sb_1__1_/config.tcl
@@ -8,8 +8,8 @@ set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/sb_1__1_/src/*.v]
 # Fill this
 set ::env(CLOCK_PERIOD) "10"
 set ::env(CLOCK_PORT) "prog_clk"
-set ::env(FP_CORE_UTIL) 0.9
-set ::env(PL_TARGET_DENSITY) 0.9
+set ::env(FP_CORE_UTIL) 0.85
+set ::env(PL_TARGET_DENSITY) 0.85
 
 
 set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(PDK_VARIANT)_config.tcl
