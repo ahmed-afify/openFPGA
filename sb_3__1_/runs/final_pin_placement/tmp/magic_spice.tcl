94ea78f7f8ff3f92c932bea34b4aa10e112c0703
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-09-01 05:48:39 -0700

    3x3_pin_place

---
+++ sb_3__1_/runs/final_pin_placement/tmp/magic_spice.tcl
@@ -0,0 +1,19 @@
+
+lef read /home/aa/Desktop/Afify/openlane-rc2/openlane/pdks/sky130A/libs.ref/sky130_fd_sc_hd/techlef/sky130_fd_sc_hd.tlef
+if {  [info exist ::env(EXTRA_LEFS)] } {
+	set lefs_in $::env(EXTRA_LEFS)
+	foreach lef_file $lefs_in {
+		lef read $lef_file
+	}
+}
+def read /openLANE_flow/designs/sb_3__1_/runs/final_pin_placement//results/routing/sb_3__1_.def
+load sb_3__1_ -dereference
+cd /openLANE_flow/designs/sb_3__1_/runs/final_pin_placement//results//magic/
+extract do local
+# extract warn all
+extract
+ext2spice lvs
+ext2spice sb_3__1_.ext
+feedback save /openLANE_flow/designs/sb_3__1_/runs/final_pin_placement//logs/magic/magic_ext2spice.feedback.txt
+# exec cp sb_3__1_.spice /openLANE_flow/designs/sb_3__1_/runs/final_pin_placement//results/magic/sb_3__1_.spice
+
