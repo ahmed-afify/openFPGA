6e2e703921ca3565ca1c45961337747b991aeb11
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-09-02 07:03:25 -0700

    sb_1__1_/

--- sb_1__1_/config.tcl
+++ sb_1__1_/config.tcl
@@ -8,11 +8,12 @@ set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/sb_1__1_/src/*.v]
 # Fill this
 set ::env(CLOCK_PERIOD) "10"
 set ::env(CLOCK_PORT) "prog_clk"
-set ::env(FP_CORE_UTIL) 0.8
-set ::env(PL_TARGET_DENSITY) 0.8
+set ::env(FP_CORE_UTIL) 0.9
+set ::env(PL_TARGET_DENSITY) 0.9
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
 
-set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(PDK_VARIANT)_config.tcl
+set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
 }
