101a2213d5f8c29bf826fa8b19467a55b1862aeb
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-09-03 11:29:34 -0700

    grid_clb/

--- grid_clb/config.tcl
+++ grid_clb/config.tcl
@@ -7,10 +7,11 @@ set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/grid_clb/src/*.v]
 
 # Fill this
 set ::env(CLOCK_PERIOD) "10"
-set ::env(CLOCK_PORT) "clk"
+set ::env(CLOCK_PORT) "prog_clk"
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)
 
 
-set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(PDK_VARIANT)_config.tcl
+set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
 if { [file exists $filename] == 1} {
 	source $filename
 }
