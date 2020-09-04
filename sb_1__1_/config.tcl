fe4b28e63e0d5800b8f3c28a8b5b5c4627ee9ac3
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-09-04 04:02:46 -0700

    3x3_pin_place

--- sb_1__1_/config.tcl
+++ sb_1__1_/config.tcl
@@ -8,9 +8,10 @@ set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/sb_1__1_/src/*.v]
 # Fill this
 set ::env(CLOCK_PERIOD) "10"
 set ::env(CLOCK_PORT) "prog_clk"
-set ::env(FP_CORE_UTIL) 0.9
-set ::env(PL_TARGET_DENSITY) 0.9
+set ::env(FP_CORE_UTIL) 80
+set ::env(PL_TARGET_DENSITY) 0.80
 set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(FP_PIN_ORDER_CFG) $::env(OPENLANE_ROOT)/designs/sb_1__1_/sb_1__1_pin.cfg
 
 
 set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
--- sb_1__1_/src/openlane_src_cells.v
+++ sb_1__1_/src/openlane_src_cells.v
@@ -15,11 +15,9 @@ module sky130_fd_sc_hd__or2_1(input A, input B, output X); endmodule
 (* blackbox *)
 module sky130_fd_sc_hd__mux2_1(input A0, input A1, input S, output X); endmodule
 (* blackbox *)
-module sky130_fd_sc_hd__dfrtp_1(input CLK, input D, input RESET_B, output Q); endmodule
-(* blackbox *)
 module sky130_fd_sc_hd__dfrbp_1(input CLK, input D, input RESET_B, output Q, output Q_N); endmodule
 (* blackbox *)
-module sky130_fd_sc_hd__dfbbp(output Q, input D, input CLK, input SET_B, input RESET_B); endmodule
+module sky130_fd_sc_hd__dfbbp_1(output Q, input D, input CLK, input SET_B, input RESET_B); endmodule
 (* blackbox *)
 module iopad(input en, input inpad, output outpad, output pad); 
 sky130_fd_sc_hd__buf_2 sky130_fd_sc_hd__buf_2_io(.A(inpad), .X(outpad));
