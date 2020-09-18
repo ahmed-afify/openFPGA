4b31b74e2da954959f7f1a08b6a09b0afb71f86c
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-09-18 12:03:23 -0700

    sb_0__0_

---
+++ hardened_components/sb_0__0_/config.tcl
@@ -0,0 +1,24 @@
+# User config
+set ::env(DESIGN_NAME) sb_0__0_
+
+# Change if needed
+set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/sb_0__0_/src/*.v]
+
+
+# Fill this
+set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "prog_clk"
+set ::env(PL_TARGET_DENSITY) 0.9
+set ::env(FP_CORE_UTIL) 90
+
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+set ::env(FP_PIN_ORDER_CFG) $::env(OPENLANE_ROOT)/designs/sb_0__0_/pinplace.cfg
+
+
+
+set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
+
+
