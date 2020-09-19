fbae0e288110d4b0481eac84e038a29ba5db2b95
ahmed-afify <ahmed-afify@aucegypt.edu>
2020-09-19 12:41:04 -0700

    sb_3_3_

---
+++ hardened_components/sb_3__3_/config.tcl
@@ -0,0 +1,21 @@
+# User config
+set ::env(DESIGN_NAME) sb_3__3_
+
+# Change if needed
+set ::env(VERILOG_FILES) [glob $::env(OPENLANE_ROOT)/designs/sb_3__3_/src/*.v]
+
+
+# Fill this
+set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "prog_clk"
+set ::env(FP_CORE_UTIL) 80
+set ::env(PL_TARGET_DENSITY) 0.80
+set ::env(CLOCK_NET) $::env(CLOCK_PORT)
+
+
+set filename $::env(OPENLANE_ROOT)/designs/$::env(DESIGN_NAME)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl
+if { [file exists $filename] == 1} {
+	source $filename
+}
+
+
